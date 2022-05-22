4c35bac34e5bf5cb82f690bfc88d12ce8b6cedeb
js20028 <68904961+js20028@users.noreply.github.com>
2022-05-22 16:47:55 +0900

    Add files via upload

---
+++ SwiftUI Project/SwiftuiPractice/SwiftuiPractice/SwiftuiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiPracticeApp.swift
+//  SwiftuiPractice
+//
+//  Created by 곽재선 on 2022/05/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
